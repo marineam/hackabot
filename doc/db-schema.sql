236b2d92b20212a0315ab1777ac92f414cac63e3
Michael Marineau <mike@marineau.org>
2006-08-28 08:28:09 +0000

    add initial stats command, tweek log database to improve stats, and update the schema dump

--- doc/db-schema.sql
+++ doc/db-schema.sql
@@ -33,15 +33,44 @@ CREATE TABLE `fire` (
 ) TYPE=MyISAM;
 
 --
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `name` varchar(50) NOT NULL default '',
+  `names` varchar(200) NOT NULL default '',
+  `nick` varchar(20) default NULL,
+  `chan` varchar(20) default NULL,
+  `date` varchar(20) default NULL,
+  PRIMARY KEY  (`name`)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table `hangman`
+--
+
+CREATE TABLE `hangman` (
+  `chan` varchar(20) NOT NULL default '',
+  `state` tinyint(4) NOT NULL default '0',
+  `final` enum('win','lose') default NULL,
+  `phrase` varchar(255) NOT NULL default '',
+  `guess` varchar(255) NOT NULL default '',
+  `wrong` varchar(255) NOT NULL default '',
+  `nick` varchar(20) default NULL,
+  PRIMARY KEY  (`chan`,`state`)
+) TYPE=MyISAM;
+
+--
 -- Table structure for table `log`
 --
 
 CREATE TABLE `log` (
   `id` int(10) NOT NULL auto_increment,
-  `nick` varchar(20) NOT NULL default '',
+  `nick` varchar(20) default NULL,
   `chan` varchar(20) default NULL,
-  `text` varchar(255) default NULL,
-  `type` enum('msg','action','notice','join','part','quit') NOT NULL default 'msg',
+  `text` text,
+  `num` int(11) default NULL,
+  `type` enum('msg','action','notice','join','part','quit','stats','topic') NOT NULL default 'msg',
   `date` varchar(20) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
@@ -87,6 +116,20 @@ CREATE TABLE `score` (
 ) TYPE=MyISAM;
 
 --
+-- Table structure for table `tard`
+--
+
+CREATE TABLE `tard` (
+  `name` varchar(50) NOT NULL default '',
+  `tard` tinyint(1) NOT NULL default '0',
+  `value` int(10) NOT NULL default '0',
+  `nick` varchar(20) default NULL,
+  `chan` varchar(20) default NULL,
+  `date` varchar(20) default NULL,
+  PRIMARY KEY  (`name`)
+) TYPE=MyISAM;
+
+--
 -- Table structure for table `topic`
 --
 
@@ -107,8 +150,8 @@ CREATE TABLE `topic` (
 
 CREATE TABLE `wtf` (
   `id` int(10) NOT NULL auto_increment,
-  `acronym_i` varchar(20) NOT NULL default '',
-  `acronym` varchar(20) NOT NULL default '',
+  `acronym_i` varchar(50) NOT NULL default '',
+  `acronym` varchar(50) NOT NULL default '',
   `text` varchar(255) NOT NULL default '',
   `nick` varchar(20) default NULL,
   `chan` varchar(20) default NULL,
