d3b156683d19eef93613909a5be3bdaae7fc3aaf
Michael Marineau <mike@marineau.org>
2008-11-30 01:21:56 +0000

    Add support for help in external commands

--- lib/python/hackabot/plugins/help.py
+++ lib/python/hackabot/plugins/help.py
@@ -1,8 +1,12 @@
 """Get help!"""
 
+import os
+from glob import glob
+
 from zope.interface import implements
 from twisted.plugin import IPlugin
-from hackabot import plugin
+
+from hackabot import conf, plugin
 
 class Help(object):
     implements(IPlugin, plugin.IHackabotPlugin)
@@ -18,10 +22,38 @@ class Help(object):
                 send = plugin.manager.commands[text].__doc__
                 if send is None:
                     send = "Command '%s' is missing a help message." % text
+            elif os.path.isfile("%s/%s" % (conf.get('commands'), text)):
+                cmd = "%s/%s" % (conf.get('commands'), text)
+                send = ""
+
+                try:
+                    fd = open(cmd)
+                    found = False
+                    for line in fd:
+                        if not found and line.startswith("##HACKABOT_HELP##"):
+                            found = True
+                        elif line.startswith("##HACKABOT_HELP##"):
+                            break
+                        elif found:
+                            send += line.lstrip("# ")
+                    fd.close()
+                except IOError:
+                    send = "Error reading command '%s'" % text
+
+                send = send.strip()
+
+                if not send:
+                    send = "Command '%s' is missing a help message." % text
             else:
                 send = "Unknown command '%s'" % text
         else:
             commands = plugin.manager.commands.keys()
+
+            for cmd in glob("%s/*" % conf.get('commands')):
+                if (os.path.isfile(cmd) and not os.path.islink(cmd)
+                        and os.access(cmd, os.X_OK)):
+                    commands.append(os.path.basename(cmd))
+
             commands.sort()
             send = "Commands: %s" % " ".join(commands)
 
