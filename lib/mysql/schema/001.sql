00e4995f6239190e8655a1d4a66d337e4b18d8e7
Michael Marineau <mmarineau@itasoftware.com>
2010-03-02 16:31:32 -0500

    Convert tabs to 4 spaces.
    Starting in the hackabot2 branch the core bot code switched from tabs to
    4 spaces for indenting. This commit converts everything that still has
    tabs over for the sake of consistency, this makes no code changes.

--- lib/mysql/schema/001.sql
+++ lib/mysql/schema/001.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `metadata` (
-	`name` VARCHAR( 20 ) NOT NULL ,
-	`value` VARCHAR( 20 ) NOT NULL ,
-	PRIMARY KEY ( `name` )
+    `name` VARCHAR( 20 ) NOT NULL ,
+    `value` VARCHAR( 20 ) NOT NULL ,
+    PRIMARY KEY ( `name` )
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
