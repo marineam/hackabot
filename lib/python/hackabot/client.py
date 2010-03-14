d3f71cc8e4d2d6edeb4f3b90dc643387b5b7db07
Michael Marineau <mike@marineau.org>
2010-03-13 20:56:48 -0500

    Improve python client code, improve email-nagios, add email-rt
    This replaces the old email-nagios script and adds a new email-rt
    script, both of which use a proper email parser so special encodings in
    the subject and such should work properly. Multi-part messages from
    nagios are also properly supported.

--- lib/python/hackabot/client.py
+++ lib/python/hackabot/client.py
@@ -2,40 +2,83 @@
 
 import os
 import sys
+import socket
 
-from hackabot.etree import ElementTree
+from hackabot import parse_config
+
+class ClientError(Exception):
+    """An error in the HB Client class"""
 
 class Client(object):
 
-    def __init__(self):
-        xml = os.getenv("HB_XML")
-        self.conf = ElementTree.fromstring(xml)
+    def __init__(self, path=None, xml=None):
+        assert not (path and xml)
 
-    def connect(self):
-        raise Exception("unimplemented")
+        if not (path or xml):
+            xml = os.environ.get("HB_XML", None)
+
+        self.conn = None
+        self.conf = parse_config(path, xml)
+
+    def connect(self, path=None):
+        if not path:
+            path = os.path.join(self.conf.get("root"), "sock")
+
+        sock = socket.socket(socket.AF_UNIX)
+        sock.connect(path)
+        # Use the file API, it is better than the sockets.
+        self.conn = sock.makefile('w+', bufsize=1)
+        sock.close()
+
+        to = self.reply_to()
+        if to:
+            ret = self.cmd("to %s" % to)
 
     def close(self):
-        raise Exception("unimplemented")
+        if self.conn:
+            self.conn.close()
+            self.conn = None
 
-    def cmd(self):
-        raise Exception("unimplemented")
+    def cmd(self, send):
+        """Send a command to the hackabot control socket"""
+
+        assert '\n' not in send
+        if not self.conn:
+            self.connect()
+
+        self.conn.write("%s\n" % send)
+        ret = self.conn.readline()
+
+        if not ret:
+            raise ClientError("Hackabot connection lost with no result")
+
+        return ret.rstrip()
+
+    def check_cmd(self, send):
+        """Raise an exception of cmd does not return 'ok'"""
+        ret = self.cmd(send)
+        if ret != "ok":
+            raise ClientError("Command failed, expected 'ok', got %r" % ret)
 
     def readall(self):
         data = sys.stdin.read()
-        return data.strip()
+        return data.rstrip()
 
     def readline(self):
         line = sys.stdin.readline()
-        return line.strip()
+        return line.rstrip()
 
     def private(self):
-        return (os.getenv("HBEV_PRIVATE", "") == "True")
+        return (os.environ.get("HBEV_PRIVATE", None) == "True")
 
     def sent_by(self):
-        return os.getenv("HBEV_SENT_BY", "")
+        return os.environ.get("HBEV_SENT_BY", None)
 
     def sent_to(self):
-        return os.getenv("HBEV_SENT_TO", "")
+        return os.environ.get("HBEV_SENT_TO", None)
+
+    def reply_to(self):
+        return os.environ.get("HBEV_REPLY_TO", None)
 
     def channel(self):
         if self.private():
